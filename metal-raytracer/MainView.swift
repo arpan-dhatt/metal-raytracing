e0d3ed5fad7af191101aa74ea3ea46ba512b31d1
Arpan Dhatt <ary.dhatt@gmail.com>
2022-04-17 17:28:25 -0500

    using megakernal approach, added sky color

--- metal-raytracer/MainView.swift
+++ metal-raytracer/MainView.swift
@@ -7,10 +7,21 @@
 
 import Foundation
 import MetalKit
+import Metal
+
+typealias float3 = SIMD3<Float>
+
+struct Uniforms {
+    var origin: float3
+    var upper_left: float3
+    var horizontal: float3
+    var vertical: float3
+}
 
 class MainView : MTKView {
     var commandQueue: MTLCommandQueue!
-    var clearPass: MTLComputePipelineState!
+    var rayPass: MTLComputePipelineState!
+    var uniforms: Uniforms!
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
@@ -22,10 +33,10 @@ class MainView : MTKView {
         self.commandQueue = device?.makeCommandQueue()
         
         let library = device?.makeDefaultLibrary()
-        let clearFunc = library?.makeFunction(name: "clear_pass")
+        let rayFunc = library?.makeFunction(name: "ray_pass")
         
         do {
-            clearPass = try device?.makeComputePipelineState(function: clearFunc!)
+            rayPass = try device?.makeComputePipelineState(function: rayFunc!)
         } catch let error as NSError {
             print(error)
         }
@@ -36,17 +47,30 @@ extension MainView {
     override func draw(_ dirtyRect: NSRect) {
         guard let drawable = self.currentDrawable else { return }
         
+        // calculate uniforms
+        let focal_distance: Float = 1.0;
+        let aspect_ratio = Float(drawable.texture.width) / Float(drawable.texture.height)
+        let viewport_height: Float = 2.0;
+        let viewport_width: Float = aspect_ratio * viewport_height;
+        let uniforms = Uniforms(
+            origin: simd_float3.zero,
+            upper_left: simd_float3(x: -viewport_width / 2.0, y: viewport_height / 2.0, z: focal_distance),
+            horizontal: simd_float3(x: viewport_width / Float(drawable.texture.width), y: 0.0, z: 0.0),
+            vertical: simd_float3(x: 0.0, y: viewport_height / Float(drawable.texture.height), z: 0.0)
+        )
+        
         let commandBuffer = commandQueue.makeCommandBuffer()
         let computeCommandEncoder = commandBuffer?.makeComputeCommandEncoder()
+        computeCommandEncoder?.setBytes([uniforms], length: MemoryLayout<Uniforms>.size, index: 0)
         computeCommandEncoder?.setTexture(drawable.texture, index: 0)
         
-        let w = clearPass.threadExecutionWidth
-        let h = clearPass.maxTotalThreadsPerThreadgroup / w
+        let w = rayPass.threadExecutionWidth
+        let h = rayPass.maxTotalThreadsPerThreadgroup / w
         
         let threadsPerGrid = MTLSizeMake(drawable.texture.width, drawable.texture.height, 1)
         let threadsPerThreadgroup = MTLSize(width: w, height: h, depth: 1)
         
-        computeCommandEncoder?.setComputePipelineState(clearPass)
+        computeCommandEncoder?.setComputePipelineState(rayPass)
         computeCommandEncoder?.dispatchThreads(threadsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
         
         computeCommandEncoder?.endEncoding()
--- metal-raytracer/Shaders.metal
+++ metal-raytracer/Shaders.metal
@@ -8,8 +8,34 @@
 #include <metal_stdlib>
 using namespace metal;
 
-kernel void clear_pass(texture2d<half, access::write> out [[ texture(0) ]],
-                       uint2 gid [[thread_position_in_grid]]) {
-    if((gid.x >= out.get_width()) || (gid.y >= out.get_height())) { return; }
-    out.write(vec<half, 4>(1, 0, 0, 0), gid);
+struct Ray {
+    half3 pos;
+    half3 dir;
+};
+
+struct Uniforms {
+    float3 origin;
+    float3 upper_left;
+    float3 horizontal;
+    float3 vertical;
+};
+
+Ray create_ray(constant Uniforms *unifs, uint2 uv) {
+    return {
+        half3(unifs->origin),
+        half3(unifs->upper_left + unifs->horizontal * uv.x - unifs->vertical * uv.y - unifs->origin)
+    };
+}
+
+half4 ray_color(Ray r) {
+    half3 unit_dir = r.dir / length(r.dir);
+    half t = 0.5 * (unit_dir.y + 1.0);
+    return (1.0-t)*half4(1.0, 1.0, 1.0, 0.0) + t*half4(0.5, 0.7, 1.0, 0.0);
+}
+
+kernel void ray_pass(texture2d<half, access::write> out [[ texture(0) ]],
+                     constant Uniforms *unifs [[ buffer(0) ]],
+                     uint2 gid [[thread_position_in_grid]]) {
+    Ray r = create_ray(unifs, gid);
+    out.write(ray_color(r), gid);
 }
