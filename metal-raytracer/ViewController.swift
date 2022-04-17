f308345255e7d1d7e35d3f9cbc8e7fb891fe6783
Arpan Dhatt <ary.dhatt@gmail.com>
2022-04-17 15:35:21 -0500

    Initial Commit

---
+++ metal-raytracer/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  metal-raytracer
+//
+//  Created by Arpan Dhatt on 4/17/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
