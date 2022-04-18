38401b6d579835239c2a0a7167ee8ac30b3d3275
Arpan Dhatt <ary.dhatt@gmail.com>
2022-04-18 16:51:40 -0500

    sphere abstraction; random buffers

--- metal-raytracer/MainView.swift
+++ metal-raytracer/MainView.swift
@@ -10,13 +10,19 @@ import MetalKit
 import Metal
 
 typealias float3 = SIMD3<Float>
+typealias float4 = SIMD4<Float>
 
 struct Uniforms {
     var origin: float3
-    var sphere_center: float3
+    var sphere_count: UInt32
     var upper_left: float3
     var horizontal: float3
     var vertical: float3
+}
+
+struct Sphere {
+    var center: float3
+    var radius: Float
 }
 
 class MainView : MTKView {
@@ -24,6 +30,9 @@ class MainView : MTKView {
     var rayPass: MTLComputePipelineState!
     var uniforms: Uniforms!
     var frameCount: UInt64!
+    
+    var randFloatBuf: MTLBuffer!
+    var randUnitSphere: MTLBuffer!
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
@@ -36,6 +45,9 @@ class MainView : MTKView {
         
         self.commandQueue = device?.makeCommandQueue()
         
+        self.randFloatBuf = createRandFloatBuf(length: 4096);
+        self.randUnitSphere = createRandUnitSphere(length: 4096);
+        
         let library = device?.makeDefaultLibrary()
         let rayFunc = library?.makeFunction(name: "ray_pass")
         
@@ -45,6 +57,26 @@ class MainView : MTKView {
             print(error)
         }
     }
+    
+    func createRandFloatBuf(length: Int) -> MTLBuffer? {
+        var floats = Array.init(repeating: Float(0.0), count: length);
+        for i in 0..<length {
+            floats[i] = Float.random(in: 0..<1.0);
+        }
+        return device?.makeBuffer(bytes: floats, length: MemoryLayout<Float>.stride * floats.count, options: .storageModeShared)
+    }
+    
+    func createRandUnitSphere(length: Int) -> MTLBuffer? {
+        var float3s = Array.init(repeating: float3.zero, count: length)
+        for i in 0..<length {
+            var val = float3.random(in: 0..<1.0);
+            while (simd_length(val) > 1.0) {
+                val = float3.random(in: 0..<1.0);
+            }
+            float3s[i] = val;
+        }
+        return device?.makeBuffer(bytes: float3s, length: MemoryLayout<float3>.stride * float3s.count, options: .storageModeShared)
+    }
 }
 
 extension MainView {
@@ -52,13 +84,19 @@ extension MainView {
         guard let drawable = self.currentDrawable else { return }
         
         // calculate uniforms
-        let focal_distance: Float = 1.0;
+        let focal_distance: Float = 2.0;
         let aspect_ratio = Float(drawable.texture.width) / Float(drawable.texture.height)
         let viewport_height: Float = 2.0;
         let viewport_width: Float = aspect_ratio * viewport_height;
+        
+        let spheres = [
+            Sphere(center: float3(x: sin(Float(frameCount) / 60) * 2.0, y: cos(Float(frameCount) / 60) * 2.0, z: 5.0), radius: 1.0),
+            Sphere(center: float3(x: 0.0, y: -51.0, z: 5.0), radius: 50.0)
+        ];
+        
         let uniforms = Uniforms(
             origin: float3.zero,
-            sphere_center: float3(x: sin(Float(frameCount) / 60), y: 0.0, z: 5.0),
+            sphere_count: UInt32(spheres.count),
             upper_left: simd_float3(x: -viewport_width / 2.0, y: viewport_height / 2.0, z: focal_distance),
             horizontal: simd_float3(x: viewport_width / Float(drawable.texture.width), y: 0.0, z: 0.0),
             vertical: simd_float3(x: 0.0, y: viewport_height / Float(drawable.texture.height), z: 0.0)
@@ -68,6 +106,9 @@ extension MainView {
         let commandBuffer = commandQueue.makeCommandBuffer()
         let computeCommandEncoder = commandBuffer?.makeComputeCommandEncoder()
         computeCommandEncoder?.setBytes([uniforms], length: MemoryLayout<Uniforms>.size, index: 0)
+        computeCommandEncoder?.setBytes(spheres, length: MemoryLayout<Sphere>.stride * spheres.count, index: 1)
+        computeCommandEncoder?.setBuffer(randFloatBuf, offset: 0, index: 2)
+        computeCommandEncoder?.setBuffer(randUnitSphere, offset: 0, index: 3)
         computeCommandEncoder?.setTexture(drawable.texture, index: 0)
         
         let w = rayPass.threadExecutionWidth
--- metal-raytracer/Shaders.metal
+++ metal-raytracer/Shaders.metal
@@ -13,9 +13,19 @@ struct Ray {
     float3 dir;
 };
 
+struct Sphere {
+    float3 center;
+    float radius;
+};
+
+struct Hit {
+    float3 norm;
+    float t;
+};
+
 struct Uniforms {
     float3 origin;
-    float3 sphere_center;
+    uint sphere_count;
     float3 upper_left;
     float3 horizontal;
     float3 vertical;
@@ -32,46 +42,60 @@ void create_rays(constant Uniforms *unifs, uint2 uv, thread Ray* rays) {
     for (uint i = 0; i < 2; i++) {
         for (uint j = 0; j < 2; j++) {
             float3 offset = 0.333 * unifs->horizontal * i - 0.333 * unifs->vertical * j;
+            float3 dir = unifs->upper_left + unifs->horizontal * uv.x - unifs->vertical * uv.y - unifs->origin + offset;
             rays[i * 2 + j] = {
                 unifs->origin,
-                unifs->upper_left + unifs->horizontal * uv.x - unifs->vertical * uv.y - unifs->origin + offset
+                dir / length(dir)
             };
         }
     }
 }
 
-float hit_sphere(float3 center, float radius, thread const Ray& r) {
+Hit hit_sphere(float3 center, float radius, thread const Ray& r) {
     float3 oc = r.pos - center;
     float a = dot(r.dir, r.dir);
     float b = 2.0 * dot(oc, r.dir);
     float c = dot(oc, oc) - radius * radius;
     float disc = b*b - 4*a*c;
     if (disc < 0) {
-        return -1.0;
+        return { float3(), -1.0 };
     } else {
-        return (-b - sqrt(disc)) / (2.0 * a);
+        float t = (-b - sqrt(disc)) / (2.0 * a);
+        float3 norm = r.pos + r.dir * t - center;
+        return { norm / length(norm), t };
     }
 }
 
-half3 ray_color(constant Uniforms *unifs, thread const Ray& r) {
-    float t = hit_sphere(unifs->sphere_center, 1.0, r);
-    float3 unit_dir = r.dir / length(r.dir);
-    if (t > 0.0) {
-        float3 norm = unit_dir * t + r.pos;
-        return 0.5 * (half3(norm.x, norm.y, norm.z) + 1.0);
+half3 ray_color(constant Uniforms *unifs,
+                constant Sphere *spheres,
+                device float *rand_floats,
+                device float3 *rand_units,
+                uint id,
+                Ray r) {
+    Hit hit = { float3(0, 0, 0), -1.0 };
+    for (uint i = 0; i < unifs->sphere_count; i++) {
+        Hit temp = hit_sphere(spheres[i].center, spheres[i].radius, r);
+        if (temp.t > 0.0 && (hit.t == -1.0 || temp.t < hit.t)) hit = temp;
     }
-    t = 0.5 * (unit_dir.y + 1.0);
-    return (1.0-t)*half3(1.0, 1.0, 1.0) + t*half3(0.5, 0.7, 1.0);
+    if (hit.t > 0.0) {
+        return 0.5 * (half3(hit.norm) + 1.0);
+    }
+    hit.t = 0.5 * (r.dir.y + 1.0);
+    return (1.0-hit.t)*half3(1.0, 1.0, 1.0) + hit.t*half3(0.5, 0.7, 1.0);
 }
 
 kernel void ray_pass(texture2d<half, access::write> out [[ texture(0) ]],
                      constant Uniforms *unifs [[ buffer(0) ]],
+                     constant Sphere *spheres [[ buffer(1) ]],
+                     device float *rand_floats [[ buffer(2) ]],
+                     device float3 *rand_units [[ buffer(3) ]],
                      uint2 gid [[thread_position_in_grid]]) {
     Ray rays[4];
     create_rays(unifs, gid, rays);
     half3 col = half3();
     for (uint i = 0; i < 4; i++) {
-        col += 0.25 * ray_color(unifs, rays[i]);
+        uint id = gid.x * out.get_width() + gid.y + i * 719;
+        col += 0.25 * ray_color(unifs, spheres, rand_floats, rand_units, id & 4095, rays[i]);
     }
     out.write(half4(col, 1.0), gid);
 }
